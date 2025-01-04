c50162ea6feb12a0bd1648932ce5e2c737d2abb0
Ali5Faridi <ali.faridi@dci-student.org>
2025-01-04 15:01:53 +0100

    #

---
+++ client/src/Post.js
@@ -0,0 +1,33 @@
+export default function Post() {
+    return (
+        <div>
+        <div className="post">
+          <div className="image">
+            <img
+              src="https://techcrunch.com/wp-content/uploads/2024/12/getty-2025-new-year-times-square.jpg?resize=708,472"
+              alt=""
+            />
+          </div>
+          <div className="texts">
+            <h2>
+              New Year’s cybersecurity resolutions that every startup should
+              keep
+            </h2>
+            <p className = "info">
+              <a className = "author">Ali Faridi</a>
+              <time>2025-01-04  12:42</time>
+
+            </p>
+            <p className = "summary">
+              It’s a new year, and that means it’s time to make some
+              resolutions. If you’re a startup, one of the best resolutions you
+              can make is to improve your cybersecurity. Cybersecurity is a
+              critical part of running a business, and it’s something that every
+              startup should take seriously. Here are some cybersecurity
+              resolutions that every startup should keep in 2025.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+}
