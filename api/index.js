0792d0b8efe5c6453ab13d9618bd6d91bd36bad0
Ali5Faridi <ali.faridi@dci-student.org>
2025-01-05 12:26:22 +0100

    added mongo server

--- api/index.js
+++ api/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://AliFaridi:zJmuPEhGAlJFLtNM@cluster0.fc1oy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 app.post('/register', (req, res) => {
-    res.json('Hello from the API');
+    const { username, password } = req.body;
+    res.json({requestData:{username, password}});
     });
 
 app.listen(4000);
+
+
+// zJmuPEhGAlJFLtNM
+
+// mongodb+srv://AliFaridi:zJmuPEhGAlJFLtNM@cluster0.fc1oy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
---
+++ api/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 4
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 4
+    }
+    
+});
+
+const UserModel = model('User', userSchema);
+
+module.exports = UserModel;
