0b12af26461aecaf0915f42b5d21cbe154a6a8be
Ali5Faridi <ali.faridi@dci-student.org>
2025-01-08 09:25:06 +0100

    made login

--- api/index.js
+++ api/index.js
@@ -1,42 +1,150 @@
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const User = require('./models/User');
+// const bcrypt = require('bcryptjs');
+// const app = express();
+// const jwt = require('jsonwebtoken');
+// const dotenv = require('dotenv');
+// const cookieParser  = require('body-parser');
+// dotenv.config();
+
+
+// const salt = bcrypt.genSaltSync(10);
+// const secret = process.env.SECRET
+
+
+// app.use(cors({Credentials: true,Origin:'http://localhost:3000'}));
+// app.use(express.json());
+
+// mongoose.connect(process.env.MONGO_URI)
+
+
+// app.post('/register', async (req, res) => {
+//     const { username, password } = req.body;
+//     try{
+//     const userDoc = await User.create({
+//         username,
+//         password: bcrypt.hashSync(password, salt),
+//         });
+//     res.json(userDoc);
+//     }catch(e){
+//         console.log(e);
+//         res.status(400).json({e});
+//     }
+    
+//     });
+    
+
+// //   login
+
+// app.post('/login', async (req,res) => {
+//     const {username,password} = req.body;
+//     const userDoc = await User.findOne({username});
+//     const passOk = bcrypt.compareSync(password, userDoc.password);
+//     if (passOk) {
+//       // logged in
+//       jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+//         if (err) throw err;
+//         res.cookie('token', token).json({
+//           id:userDoc._id,
+//           username,
+//         });
+//       });
+//     } else {
+//       res.status(400).json('wrong credentials');
+//     }
+//   });
+  
+//   app.get('/profile', (req,res) => {
+//     const {token} = req.cookies;
+//     jwt.verify(token, secret, {}, (err,info) => {
+//       if (err) throw err;
+//       res.json(info);
+//     });
+//   });
+  
+//   app.post('/logout', (req,res) => {
+//     res.cookie('token', '').json('ok');
+//   });
+
+// app.listen(4000);
+
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');  // اصلاح: استفاده از cookie-parser
+dotenv.config();
+
 const app = express();
-
 const salt = bcrypt.genSaltSync(10);
+const secret = process.env.SECRET;
 
-
-
-
-app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
+app.use(cookieParser());  // استفاده از cookie-parser برای خواندن کوکی‌ها
 
-mongoose.connect('mongodb+srv://tec-blog:lXuBJp8EhFpEouKH@cluster0.hevwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-    try{
+  const { username, password } = req.body;
+  try {
     const userDoc = await User.create({
-        username,
-        password: bcrypt.hashSync(password, salt),
-        });
-    res.json(userDoc);
-    }catch(e){
-        console.log(e);
-        res.status(400).json({e});
-    }
-    
+      username,
+      password: bcrypt.hashSync(password, salt),
     });
+    res.json(userDoc);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ e });
+  }
+});
 
-app.listen(4000);
+// login
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
 
+  if (!userDoc) {
+    return res.status(400).json('User not found');
+  }
 
-// zJmuPEhGAlJFLtNM
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token, { httpOnly: true }).json({
+        id: userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json('Wrong credentials');
+  }
+});
 
-// mongodb+srv://AliFaridi:zJmuPEhGAlJFLtNM@cluster0.fc1oy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+app.get('/profile', (req, res) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return res.status(401).json('No token, authorization denied');
+  }
 
-// lXuBJp8EhFpEouKH
-// mongodb+srv://tec-blog:lXuBJp8EhFpEouKH@cluster0.hevwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) return res.status(401).json('Invalid token');
+    res.json(info);
+  });
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('token', '', { expires: new Date(0) }).json('ok');
+});
+
+app.listen(4000, () => {
+  console.log('Server is running on http://localhost:4000');
+});
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,12 +1,100 @@
-export default function LoginPage() {
-    return (
+// import { useState } from 'react';
+// import { Navigate } from 'react-router-dom';
+
+// export default function LoginPage() {
+//     const [username, setUsername] = useState("");
+//     const [password, setPassword] = useState("");
+//     const [redirect, setRedirect] = useState(false);
+
+//     async function login(ev) {
+//         ev.preventDefault();
+//         const response = await fetch('http://localhost:4000/login', {
+//           method: 'POST',
+//           body: JSON.stringify({username, password}),
+//           headers: {'Content-Type':'application/json'},
+//           credentials: 'include',
+//         });
+//         if (response.ok) {
+//           response.json().then(userInfo => {
+//             setUserInfo(userInfo);
+//             setRedirect(true);
+//           });
+//         } else {
+//           alert('wrong credentials');
+//         }
+//       }
+    
+//       if (redirect) {
+//         return <Navigate to={'/'} />
+//       }
+
+//     return (
            
-            <form className="login">
-            <h1>Login</h1>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Login</button>
-            </form>
+//         <form className="login" onSubmit={login}>
+//         <h1>Login</h1>
+//         <input type="text"
+//                placeholder="username"
+//                value={username}
+//                onChange={ev => setUsername(ev.target.value)}/>
+//         <input type="password"
+//                placeholder="password"
+//                value={password}
+//                onChange={ev => setPassword(ev.target.value)}/>
+//         <button>Login</button>
+//       </form>
        
+//     )
+// }
+
+
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
+export default function LoginPage() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [redirect, setRedirect] = useState(false);
+    const [userInfo, setUserInfo] = useState(null); // اضافه کردن state برای ذخیره اطلاعات کاربر
+
+    async function login(ev) {
+        ev.preventDefault();
+        const response = await fetch('http://localhost:4000/login', {
+          method: 'POST',
+          body: JSON.stringify({username, password}),
+          headers: {'Content-Type':'application/json'},
+          credentials: 'include',
+        });
+
+        if (response.ok) {
+          response.json().then(userInfo => {
+            setUserInfo(userInfo); // ذخیره اطلاعات کاربر
+            setRedirect(true);
+          });
+        } else {
+          alert('wrong credentials');
+        }
+    }
+
+    if (redirect) {
+        return <Navigate to={'/'} />
+    }
+
+    return (
+        <form className="login" onSubmit={login}>
+            <h1>Login</h1>
+            <input 
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={ev => setUsername(ev.target.value)} 
+            />
+            <input 
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={ev => setPassword(ev.target.value)} 
+            />
+            <button>Login</button>
+        </form>
     )
 }
